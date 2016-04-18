8910d33e6c1c33e2fe7dcb62ce27859c6a59264c
chansuke <moonset20@gmail.com>
2016-04-18 22:58:13 +0900

    Add encryption

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,6 +1,11 @@
 defmodule Mamoblog.SessionController do
   use Mamoblog.Web, :controller
+  alias Mamoblog.User
+  import Comonin.Bcrypt, only: [checkw: 2]
+
+  plug :scrub_params, "user" when action in [:create]
+
   def new(conn, _params) do
-    render conn, "new.html"
+    render conn, "new.html", changeset: User.changeset(%User{})
   end
 end
